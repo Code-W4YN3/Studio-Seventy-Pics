1e84b70786480d9af8934bc1fafd59b661304055
Wayne Otido <otidowayne@gmail.com>
2023-08-04 01:36:55 +0300

    further photopage styling

--- src/photoPage.js
+++ src/photoPage.js
@@ -6,7 +6,8 @@ function PhotoPage({ photos, selectedPhoto, setSelectedPhoto }){
   
   return(
     <>
-      <h1>Sample Photos</h1>
+      <h1 id="photoTitle"><i>Photo</i> <b>Collection</b></h1>
+      <h2 id="photoH2">Sample Photos</h2>
       <div className="selectedPhotoDiv">
         <a href={selectedPhoto}><img className="selectedPhoto" src={selectedPhoto} alt=" " /></a>
       </div>
